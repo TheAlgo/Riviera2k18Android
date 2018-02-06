a8afd62132929dd1794c8917aa6dd2dfcc4762f3
YK-Yash <yashkumar1998@gmail.com>
2018-02-06 13:35:20 +0530

    Added recycler View

--- app/src/main/java/com/example/android/riviera/MainActivity.java
+++ app/src/main/java/com/example/android/riviera/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
--- app/src/main/java/com/example/android/riviera/fragments/Day1Fragment.java
+++ app/src/main/java/com/example/android/riviera/fragments/Day1Fragment.java
@@ -3,14 +3,26 @@ package com.example.android.riviera.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.riviera.Adapters.RecyclerAdapter;
+import com.example.android.riviera.Models.TestingModel;
 import com.example.android.riviera.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Day1Fragment extends Fragment {
+
+    RecyclerView recyclerView;
+    List<TestingModel> list= new ArrayList<>();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +33,19 @@ public class Day1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_day3, container, false);
+        View view =  inflater.inflate(R.layout.fragment_day1, container, false);
+
+        recyclerView = (RecyclerView)view.findViewById(R.id.recycle);
+        TestingModel model = new TestingModel("ABC");
+        list.add(model);
+
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(list, getActivity().getApplicationContext());
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity().getApplicationContext(),1);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator( new DefaultItemAnimator());
+        recyclerView.setAdapter(recyclerAdapter);
+
+        return view;
     }
 
 
