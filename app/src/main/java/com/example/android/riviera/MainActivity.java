7022ea89387690bc542b5b779e8c0344aee9fc4c
TheAlgo <dhirajkumar.jain2016@vitstudent.ac.in>
2018-02-06 09:24:18 +0530

    Changes

--- app/src/main/java/com/example/android/riviera/MainActivity.java
+++ app/src/main/java/com/example/android/riviera/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_events) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_favourites) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_categories) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_results) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        
 
         }
 
