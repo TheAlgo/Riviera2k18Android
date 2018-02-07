ea3df285c71c8d68592360c63b773e69d808a241
YK-Yash <yashkumar1998@gmail.com>
2018-02-07 12:47:16 +0530

    Got all fragments running

--- app/src/main/java/com/example/android/riviera/MainActivity.java
+++ app/src/main/java/com/example/android/riviera/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.riviera;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,7 +8,9 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,6 +20,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 
@@ -100,6 +105,31 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_events) {
+
+            CharSequence[] items = {"Filter 1", "Filter 2"};
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int item) {
+
+                    if(item == 0) {
+
+                    } else if(item == 1) {
+
+                    } else if(item == 2) {
+
+                    }
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            WindowManager.LayoutParams wmlp = dialog.getWindow().getAttributes();
+
+            wmlp.gravity = Gravity.BOTTOM | Gravity.CENTER;
+            wmlp.x = 0;   //x position
+            wmlp.y = 0;   //y position
+
+            dialog.show();
             // Handle the camera action
         } else if (id == R.id.nav_favourites) {
 
